3ce926c7e8a7adebf4920abc8fcac6cc6a4aa6d9
Oleg <oniziev@legendcity.app>
2022-09-19 02:52:43 +0300

    Правка роутера

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { HomePage } from './components/home/HomePage';
 import { ErrorPage } from './components/errorPage/ErrorPage';
 import { EmptyBoard } from './components/empty/EmptyBoard';
-import { Board } from './components/main/board/Board';
 import { Structure } from './components/main/board/structure/Structure';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}>
-        <Route index element={<Board />} />
-        <Route path="empty" element={<EmptyBoard />} />
+        <Route index element={<EmptyBoard />} />
+        <Route path="/empty" element={<EmptyBoard />} />
         <Route path="/structure" element={<Structure />} />
       </Route>
-
       <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
